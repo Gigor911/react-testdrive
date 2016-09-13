aacfeb4d464b89042ebbeda77c911766ad1e1301
Ihor Parkhomenko <gigor911@gmail.com>
2016-09-13 11:34:35 +0300

    first tutorial

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,10 +1,12 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var config = {
     devtool: 'cheap-module-eval-source-map',
     entry: [
         './app.js',
+        './app.scss',
         'webpack-hot-middleware/client'
     ],
     output: {
@@ -18,12 +20,23 @@ var config = {
                 test: /\.js$/,
                 loaders: ['babel'],
                 exclude: /node_modules/
-            }
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
         ]
     },
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin('styles.css', {
+            allChunks: true
+        })
     ]
 
 };
